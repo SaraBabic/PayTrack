0451c544eb813b5eea9b5c4997d7abd29cfc304d
Sara <sara_babic@intention.de>
2025-05-24 20:06:52 +0200

    add edit and delete for customer

--- routes/customer.js
+++ routes/customer.js
@@ -3,7 +3,23 @@ import Customer from "../models/customer.js";
 
 const router = express.Router();
 
-// Get all customers
+// Middleware fot get customer with ID
+const getCustomer = async (req, res, next) => {
+  let customer;
+  try {
+    customer = await Customer.findById(req.params.id);
+    if (!customer) {
+      return res.status(404).json({ message: "Korisnik nije pronađen" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.customer = customer;
+  next();
+};
+
+// GET all customers
 router.get("/", async (req, res) => {
   try {
     const customers = await Customer.find();
@@ -13,7 +29,12 @@ router.get("/", async (req, res) => {
   }
 });
 
-// Create a customer
+// GET one customer
+router.get("/:id", getCustomer, (req, res) => {
+  res.json(res.customer);
+});
+
+// POST new customer
 router.post("/", async (req, res) => {
   const customer = new Customer({ name: req.body.name });
   try {
@@ -21,6 +42,33 @@ router.post("/", async (req, res) => {
     res.status(201).json(newCustomer);
   } catch (err) {
     res.status(400).json({ message: err.message });
+  }
+});
+
+// PUT - edit customer
+router.put("/:id", getCustomer, async (req, res) => {
+  if (req.body.name != null) {
+    res.customer.name = req.body.name;
+  }
+
+  try {
+    const updatedCustomer = await res.customer.save();
+    res.json(updatedCustomer);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE - customer
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedCustomer = await Customer.findByIdAndDelete(req.params.id);
+    if (!deletedCustomer) {
+      return res.status(404).json({ message: "Korisnik nije pronađen" });
+    }
+    res.json({ message: "Korisnik obrisan" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
